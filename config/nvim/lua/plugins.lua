6d5cd8cecb5458bf4b3179bc60e35d0d3aaac7ef
Stephen Doyle <stephen.doyle@intel.com>
2024-04-23 16:07:50 +0000

    Add nvim yang plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -2,5 +2,6 @@ return {
   "folke/neodev.nvim",
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  "nathanalderson/yang.vim",
 }
