d786c22448a914c69c53e5c52d7848f8c130c69e
Stephen Doyle <stephen.doyle@intel.com>
2024-04-24 09:33:18 +0000

    Add fzf-lua to nvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -4,4 +4,13 @@ return {
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
   "nathanalderson/yang.vim",
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup({})
+    end
+  }
 }
